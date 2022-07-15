fcb2fda51b2a24d0e26d64a3830733ef2c4a3d7f
Vivek Yadav <vivekyadav36837@gmail.com>
2022-07-15 14:03:06 +0530

    add react-redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
-import  ReactDOM  from 'react-dom';
+import ReactDOM from 'react-dom';
+
+
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import reducers from './reducers/reducers.js';
 
 import App from './App';
 
-ReactDOM.render(<App/>,document.getElementById('root'));
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
+ReactDOM.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
 
