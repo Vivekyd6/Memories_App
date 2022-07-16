7774c036bb6b648b00540b07d2063f601aa5c49f
Vivek Yadav <vivekyadav36837@gmail.com>
2022-07-16 12:57:21 +0530

    create ,update , like and delete

--- client/src/actions/actions.js
+++ client/src/actions/actions.js
@@ -1,11 +1,49 @@
 import * as api from '../Api/api.js';
-
+import {FETCH,DELETE,CREATE,UPDATE,LIKE} from '../constants/actionType';
 
 export const getPosts = () => async (dispatch) => {
-    try {
-        const { data } = await api.fetchPosts();
-        dispatch({ type: 'FETCH_ALL', payload: data });
-    } catch (error) {
-        console.log(error.message);
-    }
+  try {
+    const { data } = await api.fetchPosts();
+    dispatch({ type: FETCH, payload: data.postMessages});
+  } catch (error) {
+    console.log(error);
+  }
 }
+
+export const createPost = (post) => async (dispatch) => {
+  try {
+    const { data } = await api.createPost(post);
+    dispatch({ type: CREATE, payload: data.postMessages });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const updatePost = (id, post) => async (dispatch) => {
+  try {
+    const { data } = await api.updatePost(id, post);
+    dispatch({ type: UPDATE, payload: data.postMessages });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const likePost = (id) => async (dispatch) => {
+  try {
+    const { data } = await api.likePost(id)
+    dispatch({ type: LIKE, payload: data.postMessages });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const deletePost = (id) => async (dispatch) => {
+  try {
+    await api.deletePost(id);
+    dispatch({ type: DELETE, payload: id });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+
