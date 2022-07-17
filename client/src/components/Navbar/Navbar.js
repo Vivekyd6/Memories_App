fa73e56dabd1303819c2695652131ba5d926053f
Vivek Yadav <vivekyadav36837@gmail.com>
2022-07-17 21:16:52 +0530

    Update Navbar.js

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,17 +1,36 @@
 import React from 'react';
-import { AppBar, Typography} from '@material-ui/core';
+import {Toolbar,Button,Avatar, AppBar, Typography } from '@material-ui/core';
 import memories from '../../images/memories.png';
+import useStyles from './style';
+import { Link } from 'react-router-dom';
 
 
-import style from '../../style';
+
+
 const Navbar = () => {
+  const style = useStyles();
+  const user = null;
   return (
-    <div>
-         <AppBar className={style.appBar} position="static" color="inherit">
-        <Typography className={style.heading} variant="h2" align="center">Memories</Typography>
+    <AppBar className={style.appBar} position="static" color="inherit">
+      <div className={style.brandContainer}>
+        <Typography component={Link} to="/" className={style.heading} variant="h2" align="center">Memories</Typography>
         <img className={style.image} src={memories} alt="icon" height="60" />
-      </AppBar>
-    </div>
+      </div>
+      <Toolbar className={style.toolbar}>
+        {user ? (
+          <div className={style.profile}>
+            <Avatar className={style.purple} alt={user.result.name} src={user.result.imageUrl}>
+              {user.result.name.charAt(0)}
+              <Typography className={style.userName} variant="h6">{user.result.name}</Typography>
+              <Button className={style.logout} color="secondary" variant="contained">LOGOUT</Button>
+            </Avatar>
+          </div>
+        ) : (
+          <Button component={Link} to ="/auth"  color="primary" variant="contained">Sign IN</Button>
+      )}
+      </Toolbar>
+    </AppBar>
+
   )
 }
 
