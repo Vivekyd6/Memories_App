b6db20dbeb1ae441b5b54163b66c3e1b6f6678be
Vivek Yadav <vivekyadav36837@gmail.com>
2022-07-17 21:15:34 +0530

    update

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -8,7 +8,7 @@ import { Grid, CircularProgress } from '@material-ui/core'
 const Posts = ({setCurrId}) => {
     const posts = useSelector((state) => state.posts)
     const style = useStyles();
-    //    console.log(posts);
+       console.log(posts);
     return (
         !posts.length ? <CircularProgress /> : (
             <Grid className={style.container} container alignItems="stretch" spacing={3}>
